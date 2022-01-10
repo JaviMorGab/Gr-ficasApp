2acb4d18278144e1478bd147ae92cffdb476923c
JaviMorGab <franjamoga.93@gmail.com>
2022-01-10 11:27:56 +0100

    fifth commit

--- src/app/graficas/pages/donut-http/donut-http.component.ts
+++ src/app/graficas/pages/donut-http/donut-http.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
+
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-donut-http',
@@ -8,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonutHttpComponent implements OnInit {
 
-  constructor() { }
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ], backgroundColor: '#008DE6', hoverBackgroundColor: '#804DE8' },
+      { data: [ 50, 150, 120 ], backgroundColor: '#00D2F0', hoverBackgroundColor: '#804DE8' },
+      { data: [ 250, 130, 70 ], backgroundColor: '#0BD9BA', hoverBackgroundColor: '#804DE8' },
+      { data: [ 230, 150, 50 ], backgroundColor: '#00F088', hoverBackgroundColor: '#804DE8' },
+      { data: [ 150, 330, 100 ], backgroundColor: '#00E641', hoverBackgroundColor: '#804DE8' }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
+    // this.graficasService.getUsuariosRedesSociales()
+    //   .subscribe(data => {
+    //     console.log(data);
+    //     const labels: Object.keys(data);
+    //     const values: Object.values(data);
+
+    //     this.doughnutChartLabels = labels;
+    //     this.doughnutChartData.push(values);
+    //   });
+
+    this.graficasService.getUsuariosRedesSocialesDonutData()
+       .subscribe(({labels, values}) => {
+         this.doughnutChartLabels = labels;
+         this.doughnutChartData.push(values);
+        });
   }
 
 }
--- src/app/graficas/pages/donut/donut.component.ts
+++ src/app/graficas/pages/donut/donut.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-donut',
@@ -6,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonutComponent implements OnInit {
+export class DonutComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ], backgroundColor: '#008DE6', hoverBackgroundColor: '#804DE8' },
+      { data: [ 50, 150, 120 ], backgroundColor: '#00D2F0', hoverBackgroundColor: '#804DE8' },
+      { data: [ 250, 130, 70 ], backgroundColor: '#0BD9BA', hoverBackgroundColor: '#804DE8' },
+      { data: [ 230, 150, 50 ], backgroundColor: '#00F088', hoverBackgroundColor: '#804DE8' },
+      { data: [ 150, 330, 100 ], backgroundColor: '#00E641', hoverBackgroundColor: '#804DE8' }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
 }
